472de412839ef99612fe6de01d7c0fac9c1e262d
Gustavo Gorges <gustavoleandrogorges@gmail.com>
2025-05-28 16:50:35 -0300

    feat: implementa serviço para obter disciplinas matriculadas do usuário

---
+++ lib/services/user.ts
@@ -0,0 +1,82 @@
+import { AxiosResponse } from 'axios';
+
+import api from '../api';
+import { tokenManager } from '../auth/tokenManager';
+
+interface Monitor {
+  _id: string;
+  id: string;
+}
+
+interface Student {
+  _id: string;
+  id: string;
+}
+
+interface EntityId {
+  _id: string;
+  name: string;
+  id: string;
+}
+
+interface TestItem {
+  monitors: Monitor[];
+  students: Student[];
+  hasTeacherStartedTestEarly: boolean;
+  instructions: string;
+  initialDate: string; // Or Date if you plan to parse it immediately
+  endDate: string; // Or Date if you plan to parse it immediately
+  title: string;
+  entityId: EntityId;
+  id: string;
+}
+
+// Tipo das entidades
+interface EntityAction {
+  _id: string;
+  methods: string[];
+  label: string;
+  name: string;
+  id: string;
+}
+
+// Interface para o objeto 'privileges'
+interface EntityPrivileges {
+  _id: string;
+  actions: EntityAction[];
+  label: string;
+  id: string;
+}
+
+// Interface para cada item dentro do array 'data' principal
+interface EntityDataItem {
+  id: string;
+  title: string;
+  testCount: number;
+  tests: TestItem[];
+  privileges: EntityPrivileges;
+}
+
+// Interface para o objeto que contém 'success' e o array 'data'
+interface UserSubjectsResponse {
+  success: boolean;
+  data: EntityDataItem[];
+}
+
+export const userService = {
+  async getUserEnrolledSubjects() {
+    try {
+      const response: AxiosResponse<UserSubjectsResponse> = await api.get(
+        'user/entitiesWithTests',
+        {
+          headers: tokenManager.getAuthencationHeaders(),
+        },
+      );
+
+      return response.data.data;
+    } catch (error) {
+      console.error('Error fetching user entities:', error);
+      throw error;
+    }
+  },
+};
