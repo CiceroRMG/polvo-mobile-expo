3101cdde3b2539a9e75fec502d6fa58771600c98
Gustavo Gorges <gustavoleandrogorges@gmail.com>
2025-05-29 16:19:58 -0300

    feat: Adiciona tratamento de error e loading onde não existia e deixa uma base para como realizar a requisição para o back-end

--- app/(tabs)/disciplines/[disciplineId]/index.tsx
+++ app/(tabs)/disciplines/[disciplineId]/index.tsx
@@ -1,9 +1,11 @@
 import { router, useLocalSearchParams } from 'expo-router';
 import * as React from 'react';
-import { Text, View, FlatList } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Text, View, FlatList, ActivityIndicator } from 'react-native';
 
 import { AppBackButton } from '~/components/app/AppBackButton';
 import { AppCard } from '~/components/app/AppCard';
+import { userService } from '~/lib/services/user';
 
 const mockDisciplines = [
   { id: '1', title: 'Quiz 1' },
@@ -13,11 +15,31 @@ const mockDisciplines = [
 ];
 
 export default function DisciplineDetail() {
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [subjectTests, setSubjectTests] = useState([]);
+
   const { disciplineId } = useLocalSearchParams();
 
   const handleQuizzPress = (quizzId: string) => {
     router.push(`/disciplines/${disciplineId}/${quizzId}`);
   };
+
+  useEffect(() => {
+    const fetchSubjectTests = async () => {
+      try {
+        setIsLoading(true);
+        userService.getSubjectDataById(disciplineId as string);
+      } catch (error) {
+        console.error('Failed to fetch subject tests:', error);
+        setError('Falha ao carregar suas provas tente novamente.');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchSubjectTests();
+  }, [disciplineId]);
 
   return (
     <View className="flex-1 bg-background">
@@ -33,21 +55,33 @@ export default function DisciplineDetail() {
           Atividades Disponíveis
         </Text>
 
-        <FlatList
-          data={mockDisciplines}
-          keyExtractor={item => item.id}
-          renderItem={({ item }) => (
-            <AppCard
-              onPress={() => handleQuizzPress(item.id)}
-              title={item.title}
-            />
-          )}
-          contentContainerStyle={{
-            paddingBottom: 24,
-            gap: 14,
-          }}
-          showsVerticalScrollIndicator={false}
-        />
+        {isLoading ? (
+          <ActivityIndicator size="large" color="#6200ee" />
+        ) : error ? (
+          <Text className="text-red-500 text-center my-4">{error}</Text>
+        ) : (
+          <FlatList
+            data={mockDisciplines}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+              <AppCard
+                onPress={() => handleQuizzPress(item.id)}
+                title={item.title}
+                subtitle={item.title}
+              />
+            )}
+            contentContainerStyle={{
+              paddingBottom: 24,
+              gap: 14,
+            }}
+            ListEmptyComponent={
+              <Text className="text-center text-gray-500 my-4">
+                Não há atividades disponíveis para esta disciplina.
+              </Text>
+            }
+            showsVerticalScrollIndicator={false}
+          />
+        )}
       </View>
     </View>
   );
